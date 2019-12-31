fe58ab10bf039352499f13027ae37f1168a6bbd9
Ömer TUNÇ <omer_tunc_04@hotmail.com>
2019-12-31 11:50:10 +0300

    update

--- EventManagementSystem.WebApi/App_Start/SwaggerConfig.cs
+++ EventManagementSystem.WebApi/App_Start/SwaggerConfig.cs
@@ -22,9 +22,12 @@ namespace EventManagementSystem.WebApi
 
                     c.SingleApiVersion("v1", "EventManagementSystem.WebApi");
                     c.OperationFilter<CultureAwareOperationFilter>();
+                    c.DocumentFilter<AuthTokenOperation>();
+                    c.PrettyPrint();
                 })
                 .EnableSwaggerUi(c =>
                 {
+                    c.DocumentTitle("EventManagementSystem.WebApi");
                 });
         }
     }
@@ -44,6 +47,49 @@ namespace EventManagementSystem.WebApi
                 type = "string",
                 required = true,
                 @default = "Bearer "
+            });
+        }
+    }
+    public class AuthTokenOperation : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {
+
+            swaggerDoc.paths.Add("/token", new PathItem
+            {
+                post = new Operation
+                {
+                    tags = new List<string> { "Auth" },
+                    consumes = new List<string>
+                    {
+                        "application/x-www-form-urlencoded"
+                    },
+                    parameters = new List<Parameter>
+                    {
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "grant_type",
+                            required = true,
+                            @in = "formData",
+                            @default = "password"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "UserName",
+                            required = true,
+                            @in = "formData"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "Password",
+                            required = true,
+                            @in = "formData"
+                        },
+                    }
+                }
             });
         }
     }
